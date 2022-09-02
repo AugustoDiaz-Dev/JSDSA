c36b57f3e794e0a442765503555105997412f7aa
Augusto <augustodiaz_1@hotmail.com>
2022-09-03 00:07:46 +1000

    JS DSA

--- JSDSASELECTED.js
+++ JSDSASELECTED.js
@@ -183,6 +183,116 @@ do {
 }
 while (i < 11);
 
+// Replace Loops using Recursion 
+
+function sum(arr, n) {
+  if(n <= 0) {
+    return 0;
+  } else {
+    return sum(arr, n-1) + arr[n-1]
+  }
+}
+
+// Profile Lookup 
+
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  for (let i = 0; i < contacts.length; i++) {
+    if (contacts[i].firstName === name) {
+      if (prop in contacts[i]) {
+        return contacts[i][prop];
+      } else {
+        return "No such property";
+      }
+    }
+  }
+  return "No such contact";
+}
+
+lookUpProfile("Akira", "likes");
+
+// Generate Random Whole Numbers with JavaScript 
+
+function randomWholeNum() {
+  return Math.floor(Math.random() * 10);
+}
+
+// Generate Random Whole Numbers within a Range
+
+function randomRange(myMin, myMax) {
+  return Math.floor(Math.random() * (myMax - myMin + 1) + myMin);
+}
+
+// Use the parseInt Function with a Radix 
+
+function convertToInteger(str) {
+  return parseInt(str, 2)
+ }
+ 
+ convertToInteger("10011");
+
+// Use the Conditional (Ternary) Operator
+
+function checkEqual(a, b) {
+  return a === b ? 'Equal' : 'Not Equal'
+}
+
+checkEqual(1, 2);
+
+// Use Multiple Conditional (Ternary) Operators 
+
+function checkSign(num) {
+  return num > 0 ? 'positive' 
+  : (num < 0) ? 'negative'
+  : 'zero'
+}
+
+checkSign(10);
+
+// Use Recursion to Create a Countdown
+
+function countdown(n){
+  return n < 1 ? [] : [n, ...countdown(n - 1)];
+}
+
+// Use Recursion to Create a Range of Numbers
+
+function rangeOfNumbers(startNum, endNum) {
+  if (endNum < startNum) {
+    return [];
+  } else {
+    const numbers = rangeOfNumbers(startNum, endNum - 1);
+    numbers.push(endNum);
+    return numbers;
+  }
+}
+
 // BASIC DATA STRUCTURES //
 
 // Review: 8, 10, 11, 17, 18, 19, 20.
