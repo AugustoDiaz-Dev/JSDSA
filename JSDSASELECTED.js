076326a5a11ef84e969a16055da3e8481a724bbd
Augusto <augustodiaz_1@hotmail.com>
2022-09-02 21:44:06 +1000

    JS DSA

--- JSDSASELECTED.js
+++ JSDSASELECTED.js
@@ -83,6 +83,106 @@ function cc(card) {
 
 cc(2); cc(3); cc(7); cc('K'); cc('A');
 
+// Using Objects for Lookups
+
+function phoneticLookup(val) {
+  var result = "";
+  var lookup = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank"
+  };
+  result = lookup[val];
+  return result;
+}
+
+phoneticLookup("charlie");
+
+// Record Collection 
+
+// Setup
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+function updateRecords(records, id, prop, value) {
+  // Access target album in record collection
+  const album = records[id];
+  // If value is an empty string,
+  //  delete the given prop property from the album
+  if (value === "") {
+    delete album[prop];
+  }
+  // If prop isn't tracks,
+  //  update or set that album's prop to value
+  else if (prop !== "tracks") {
+    album[prop] = value;
+  }
+  // If prop is tracks,
+  //  add value to the end of the album's existing tracks array
+  else {
+    album["tracks"] = album["tracks"] || [];
+    album["tracks"].push(value);
+  }
+  // Your function must always return the entire record collection object
+  return records;
+}
+
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+
+// Iterate Through an Array with a For Loop
+
+const myArr = [2, 3, 4, 5, 6];
+
+let total = 0;
+for (let i = 0; i < myArr.length; i++) {
+  total += myArr[i];
+}
+
+// Nesting For Loops
+
+function multiplyAll(arr) {
+  let product = 1;
+for(let i = 0; i < arr.length; i ++) {
+  for(let j = 0; j < arr[i].length; j++) {
+    product = product * arr[i][j];
+  }
+}
+  return product;
+}
+
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+
+// Iterate with JavaScript Do...While Loops 
+
+const myArray = [];
+let i = 10;
+
+do {
+ myArray.push(i);
+  i++;
+}
+while (i < 11);
+
 // BASIC DATA STRUCTURES //
 
 // Review: 8, 10, 11, 17, 18, 19, 20.
